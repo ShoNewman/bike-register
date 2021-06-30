5abd7ac9ead84a3995557ef3a770891634275496
Shoshana Newman <sho.newman@gmail.com>
2021-06-30 15:59:33 -0700

    Add fetch API to make API call
    Co-authored-by: Masha Ivy <mashaivy9l@gmail.com>
    Co-authored-by: Araceli Valdovinos <valdovinosaraceli50@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,25 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import BikeService from './js/bike-service.js';
 
-$(document).ready(function () {
-  $('#submit').click(function () {
-    let location = $('#location').val(); //Save input in city value
-    // clearFields(); 
-    //Calling function to empty input fields 
+function clearFields() {
+  $('#location').val('');
+}
 
-    let promise = BikeService.getBikeService(location); //References WeatherService object in imported function
-    promise.then(function (response) { //.then is one of three methods of the XHR object --> it handles both a results response and a rejected response
-      const body = JSON.parse(response); //If we get a response then we parse the response --> meaning turning the string into an object
-      console.log(body.bikes[0]);
-      for (let i = 0; i < body.bikes.length; i++) {
-        const item = body.bikes[i];
-        if (item.description !== null && item.stolen_location !== null) {
-          
-          $('.showBikes').append(`<p>${item.serial} : ${item.description} : ${item.stolen_location}</p>`); //shows city(var where the location is saved from user input), body.main.humidity --> pulling the data from the parsed response
-        }
+function getElements(response) {
+  if (response) {
+    for (let i = 0; i < response.bikes.length; i++) {
+      const item = response.bikes[i];
+      if (item.description !== null && item.stolen_location !== null) {
+        $('.showBikes').append(`<p>${item.serial} : ${item.description} : ${item.stolen_location}</p>`);
+      } else {
+        $('.showErrors').text(`There was an error: ${response.message}`);
       }
-    }, function (error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`); //If there we don't get a response then it displays the error message
-    });
-  });
+    }
+  }
+}
+
+async function makeApiCall(location) {
+  const response = await BikeService.getBikeService(location);
+  getElements(response);
+}
+
+$('#submit').on("click", function () {
+  let location = $('#location').val();
+  clearFields();
+  makeApiCall(location);
 });
